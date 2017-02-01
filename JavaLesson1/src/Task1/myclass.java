0ce21498578a52406fe4d248d5a5e68652467831
admin <admin@inv5272-nb.msk.i-teco.ru>
2017-02-01 11:52:23 +0300

    Rename the method

--- JavaLesson1/src/Task1/myclass.java
+++ JavaLesson1/src/Task1/myclass.java
@@ -7,16 +7,15 @@ public class myclass {
 	public static void main(String[] args)  {
 		
 	   
-		power(10);
+		fibonachi(100);
 		
 		}
 
-	public static void power(int n) {
+	public static void fibonachi(int n) {
 		long [] a = new  long [n];
 	    a[0]=1;
 	    a[1]=1;
 	    System.out.print(a[0] + " " + a[1] + " ");
-	   // System.out.println(a[1]);
 	    
 		for (int i=2;i<a.length;i++) {
 		
